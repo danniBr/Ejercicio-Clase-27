9e1c9c6a385a400f78e084ce0b15cbdbc10a7a2e
danniBr <danitza.br@gmail.com>
2018-07-31 20:55:57 -0400

    products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,14 @@
 class ProductsController < ApplicationController
+  def create
+  	@category = Category.find(params[:category_id])
+    @product = @category.products.build(product_params)
+    @product.save
+    redirect_to category_path(@product.category_id)
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :price)
+  end 
 end
